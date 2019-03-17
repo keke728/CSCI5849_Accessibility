7f66f317dd1e987652c78fdec275f7c345f587d2
keke728 <keke@Keke.local>
2019-03-17 15:46:58 -0600

    EC Low Vision

---
+++ Low_Vision/phone.js
@@ -0,0 +1,60 @@
+// Running each time the page is loaded
+$(document).ready(function() {
+  $("#csstab").click(function() {
+    $('link[href="style.css"]').attr('href', 'large-style.css');
+  });
+
+  $("#csstabnormal").click(function() {
+    $('link[href="large-style.css"]').attr('href', 'style.css');
+  });
+  
+  $("#Dialer").show();
+  $("#ContactList").hide();
+  $("#AddContact").hide();
+  $("#About").hide();
+});
+
+// Click on Dialer Tab
+$("#dialertab").click(function(){
+  $("#Dialer").show();
+  $("#ContactList").hide();
+  $("#AddContact").hide();
+  $("#About").hide();
+});
+
+// Click on Contact List Tab
+$("#contacttab").click(function(){
+  $("#ContactList").show();
+  $("#Dialer").hide();
+  $("#AddContact").hide();
+  $("#About").hide();
+});
+
+// Click on Add Contact Tab
+$("#addtab").click(function(){
+  $("#AddContact").show();
+  $("#Dialer").hide();
+  $("#ContactList").hide();
+  $("#About").hide();
+});
+
+// Click on About Tab
+$("#abouttab").click(function(){
+  $("#About").show();
+  $("#Dialer").hide();
+  $("#ContactList").hide();
+  $("#AddContact").hide();
+});
+
+// Dialing Functions
+$("#digits button").click(function() {
+  $("#dialerinput").val($("#dialerinput").val() + this.innerText);
+})
+
+$("#clearBtn").click(function() {
+  $("#dialerinput").val("");
+})
+
+/** References
+- How to create tabs: https://www.w3schools.com/howto/howto_js_tabs.asp
+**/
