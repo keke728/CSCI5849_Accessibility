685a5e2a60c3f4e38249e8ec9aa3b8980178bb87
keke728 <keke@Keke.local>
2019-04-27 10:23:01 -0600

    Fixed BGM

--- Project2/index.js
+++ Project2/index.js
@@ -1,10 +1,2 @@
 $(document).ready(function(){
 });
-
-$('#startbtn').hover(
-  function(){
-    var sound = document.getElementById('btnAudio');
-    const playPromise = sound.play();
-    if (playPromise !== null) {
-      playPromise.catch(() => {sound.play();})}
-});
